d1ca1a618409b0d55d0bf6c8de91690b5b5bd097
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2025-03-15 02:32:22 -0400

    Set up:
    - docker-compose.yaml set up
    - Use Member Entity in Repository.

--- src/main/java/com/metropolitan/member/domain/repository/MemberRepository.java
+++ src/main/java/com/metropolitan/member/domain/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.metropolitan.member.domain.repository;
 
-import com.metropolitan.member.domain.model.Member;
+import com.metropolitan.member.infrastructure.persistence.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
